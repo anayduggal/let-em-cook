88a19d89fa41ca5d2caa7746b43f0911b37edc5b
Kai Dallison <kai.dallison@student.manchester.ac.uk>
2025-03-17 12:32:11 +0000

    Make inc/config.php default

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,9 @@
 <?php
 
-define("DB_HOST", "dbhost.cs.man.ac.uk");
-define("DB_USERNAME", "r11283kd");
-define("DB_PASSWORD", "VCRLm/b0bUH2q+/Wdji3pY4dTCMf1+BjRQQZ+zUx8oA");
-define("DB_DATABASE_NAME", "2024_comp10120_y10");
+define("DB_HOST", "localhost");
+
+define("DB_USERNAME", "demo");
+
+define("DB_PASSWORD", "demo");
+
+define("DB_DATABASE_NAME", "recipes_db");
